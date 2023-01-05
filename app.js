09532cf1bf45500d9459f8539905d46485bf70a9
Artem <artem.semenov.101@gmail.com>
2023-01-05 17:08:55 +0200

    Added counter inreasing on click on button Add to cart; Added function on click on button 'to favorite'; added modal on click on button 'more details' and on scroll on 50% of document;

--- app.js
+++ app.js
@@ -1,3 +1,52 @@
+//increase cart counter on click on add to cart button
+
+const cartButtons = document.querySelectorAll(".device-add-to-cart");
+const counter = document.querySelector(".counter");
+cartButtons.forEach((el) => {
+  el.addEventListener("click", function (e) {
+    counter.innerHTML = +counter.innerHTML + 1;
+  });
+});
+
+//modal on click on more details
+const moreDetailsBtns = document.querySelectorAll(".device-more-details");
+const modalPopup = document.querySelector(".modal");
+const modalCloseBtn = document.querySelector(".btn-close");
+
+moreDetailsBtns.forEach((el) => {
+  el.addEventListener("click", function () {
+    modalPopup.classList.add("active");
+  });
+});
+
+document.addEventListener("click", function (e) {
+  if (e.target === modalPopup || e.target === modalCloseBtn) {
+    modalPopup.classList.remove("active");
+  }
+});
+
+// favorite buttons
+const favBtns = document.querySelectorAll(".favorite-button");
+
+favBtns.forEach((el) => {
+  el.addEventListener("click", function (e) {
+    el.classList.toggle("active");
+  });
+});
+
+//show modal on scroll more than 50% of document
+
+const htmlHeight = document.documentElement.offsetHeight;
+const viewportHeight = window.innerHeight;
+
+function showModalOnScroll(e) {
+  if (window.pageYOffset > htmlHeight / 2 - viewportHeight / 2) {
+    modalPopup.classList.add('active')
+    document.removeEventListener("scroll", showModalOnScroll);
+  }
+}
+document.addEventListener("scroll", showModalOnScroll);
+
 //Для картинок в слайдере - класс "swiper-lazy" и выносим путь в атрибут дата-срц
 //И добавляем див с классом "swiper-lazy-loader"
 const swiper = new Swiper(".swiper", {
