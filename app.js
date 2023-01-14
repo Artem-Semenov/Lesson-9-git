a770eb1367052fda159f997f1d446993d7223989
Artem <artem.semenov.101@gmail.com>
2023-01-14 14:59:18 +0200

    function constructor

--- app.js
+++ app.js
@@ -47,10 +47,75 @@ const viewportHeight = window.innerHeight;
 } */
 function showModalOnScroll(e) {
   if (window.scrollY >= document.body.scrollHeight / 2) {
-    modalPopup.classList.add('active')
+    modalPopup.classList.add("active");
     document.removeEventListener("scroll", showModalOnScroll);
   }
 }
 document.addEventListener("scroll", showModalOnScroll);
 
-$('.slider').slick()
+$(".slider").slick();
+
+////
+
+let decrementBtn = $(".decrement-btn")[0];
+console.log(decrementBtn);
+
+let incrementBtn = $(".increment-btn")[0];
+console.log(incrementBtn);
+
+let productQuantity = $(".item-counter-value")[0];
+console.log(productQuantity);
+
+function Counter(incrementBtn, decrementBtn, inputField) {
+  this.domRefs = {
+    incrementBtn,
+    decrementBtn,
+    inputField,
+  };
+  this.toggleButtonState = function () {
+    let count = this.domRefs.inputField.value;
+    this.domRefs.decrementBtn.disabled = count <= 1;
+    this.domRefs.incrementBtn.disabled = count >= 10;
+  };
+  this.toggleButtonState();
+
+  this.increment = function () {
+    this.domRefs.inputField.value = +this.domRefs.inputField.value + 1;
+    this.toggleButtonState();
+  };
+  this.decrement = function () {
+    this.domRefs.inputField.value = +this.domRefs.inputField.value - 1;
+    this.toggleButtonState();
+  };
+
+  this.domRefs.decrementBtn.addEventListener(
+    "click",
+    this.decrement.bind(this)
+  );
+
+  this.domRefs.incrementBtn.addEventListener(
+    "click",
+    this.increment.bind(this)
+  );
+  this.toggleButtonState();
+}
+
+const counter1 = new Counter(incrementBtn, decrementBtn, productQuantity);
+
+console.log(counter1);
+
+decrementBtn.onclick = () => {
+  $(".item-counter-value");
+};
+
+incrementBtn.onclick = () => {
+  $(".item-counter-value");
+};
+
+/* function Car(model, color, year) {
+  this.model = model;
+  this.year = year;
+  this.color = color;
+  // console.log(this);
+}
+let audi = new Car('audi', 'red', 2011) */
